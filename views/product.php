48dcd6392a863f2be2135e5dc7a85e03f91b377a
Weeedos <vit.vosol@gmail.com>
2023-06-01 20:22:01 +0200

    Added product class.

---
+++ views/product.php
@@ -0,0 +1,39 @@
+<?php
+
+class Product
+{
+    private $name;
+    private $price;
+    private $quantity;
+
+    function __construct(string $name, int $price, int $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function getName(): string{
+        return $this->name;
+    }
+
+    public function getPrice(): int{
+        return $this->price;
+    }
+
+    public function getQuantity(): int{
+        return $this->quantity;
+    }
+
+    public function setName($name): string{
+        $this->name = $name;
+     }
+
+     public function setPrice($price): int{
+        $this->price = $price;
+     }
+
+     public function setQuantity($quantity): int{
+        $this->quantity = $quantity;
+     }
+}
